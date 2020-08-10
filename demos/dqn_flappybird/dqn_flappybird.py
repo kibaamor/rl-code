572c4ed04e0850ee1be3d7d54f5f89419ce29394
KibaAmor <kibaamor@gmail.com>
2020-08-10 18:42:07 +0800

    add comment

--- demos/dqn_flappybird/dqn_flappybird.py
+++ demos/dqn_flappybird/dqn_flappybird.py
@@ -20,24 +20,39 @@ script_path = os.path.split(os.path.realpath(__file__))[0]
 pt_file = os.path.join(script_path, 'dqn_flappybird.pt')
 env = FlappyBirdWrapper()
 
+# 游戏状态的种类数
 OBS_N = env.observation_space.n
+# 游戏操作的种类数
 ACT_N = env.action_space.n
+# 隐藏层的大小
 HIDDEN_N = 128
+# 经验池的大小
 MEMORY_CAPACITY = 10000
+# 经验池预热的大小
 WARMUP_SIZE = 256
+# 每次学习时使用的经验数
 BATCH_SIZE = 128
+# target model同步的频率
 MODEL_SYNC_COUNT = 20
+# 学习率
 LEARNING_RATE = 1e-3
+# 学习的频率
 LEARN_FREQ = 8
-WEIGHT_DECAY = 0
+# 衰减因子
 GAMMA = 0.99
+# 探索的概率
 E_GREED = 0.1
+# 探索概率的减少量
 E_GREED_DEC = 1e-6
+# 探索概率的最小值
 E_GREED_MIN = 0.01
-EPISODES_NUM = 8000000
-USE_DDQN = False
+# 训练的总回合数
+EPISODES_NUM = 2000
+# 是否使用Double-Q Learning
+USE_DBQN = False
 
 
+# 模型
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
@@ -58,59 +73,79 @@ class Model(nn.Module):
         return action_val
 
 
+# 智能体
 class Agent:
     def __init__(self):
+        # 模型
         self.model = Model().to(device)
+        # 目标模型（Target Model）
         self.target_model = Model().to(device)
+        # 同步目标模型
         self.target_model.load_state_dict(self.model.state_dict())
 
+        # 经验池
         self.memory = deque(maxlen=MEMORY_CAPACITY)
-        self.optimizer = optim.Adam(self.model.parameters(
-        ), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
+        # 优化器
+        self.optimizer = optim.Adam(self.model.parameters(), lr=LEARNING_RATE)
+        # Loss函数
         self.loss_func = nn.MSELoss()
 
+        # 初始化探索概率
         self.e_greed = E_GREED
+        # 更新次数的计数
         self.update_count = 0
 
     def update_egreed(self):
+        # 更新探索概率
         self.e_greed = max(E_GREED_MIN, self.e_greed - E_GREED_DEC)
 
     def predict(self, obs):
+        # 根据状态预测动作
         obs = torch.FloatTensor(obs).to(device)
         q_val = self.model(obs).cpu().detach().numpy()
+        # 需要注意的是：如果多个动作的Q值都是最大时，要从这些中随机选一个
         q_max = np.max(q_val)
         choice_list = np.where(q_val == q_max)[0]
         return np.random.choice(choice_list)
 
     def sample(self, obs):
+        # 采样动作
         if np.random.rand() < self.e_greed:
+            # 探索
             return np.random.choice(ACT_N)
+        # 利用已有经验
         return self.predict(obs)
 
     def store_transition(self, trans):
+        # 保存经验
         self.memory.append(trans)
 
     def learn(self, writer):
+        # 根据经验进行学习
+
+        # 确保经验池里的经验以及足够多时，才进行学习
         assert WARMUP_SIZE >= BATCH_SIZE
         if len(self.memory) < WARMUP_SIZE:
             return
 
+        # 从经验池中选取BATCH_SIZE条经验出来
         batch = random.sample(self.memory, BATCH_SIZE)
         batch = Transition(*(zip(*batch)))
+        # 把这些经验都转换位Tensor
         s0 = torch.FloatTensor(batch.state).to(device)
         a0 = torch.LongTensor(batch.action).to(device).unsqueeze(1)
         r1 = torch.FloatTensor(batch.reward).to(device)
         s1 = torch.FloatTensor(batch.next_state).to(device)
         d1 = torch.LongTensor(batch.done).to(device)
 
+        # DQN算法
         q_pred = self.model(s0).gather(1, a0).squeeze()
         with torch.no_grad():
-            if USE_DDQN:
+            if USE_DBQN:
                 acts = self.model(s1).max(1)[1].unsqueeze(1)
                 q_target = self.target_model(s1).gather(1, acts).squeeze(1)
             else:
                 q_target = self.target_model(s1).max(1)[0]
-
             q_target = r1 + GAMMA * (1 - d1) * q_target
         loss = self.loss_func(q_pred, q_target)
 
@@ -118,73 +153,109 @@ class Agent:
         loss.backward()
         self.optimizer.step()
 
+        # 记录每次学习的loss
         writer.add_scalar('train/value_loss', loss.item(), self.update_count)
         self.update_count += 1
+        # 每MODEL_SYNC_COUNT同步一下目标模型
         if self.update_count % MODEL_SYNC_COUNT == 0:
             self.target_model.load_state_dict(self.model.state_dict())
 
     def save(self):
+        # 保存模型文件
         torch.save(self.model.state_dict(), pt_file)
         print(pt_file + ' saved.')
 
     def load(self):
+        # 加载模型文件
         self.model.load_state_dict(torch.load(pt_file))
+        # 目标模型要同步一下
         self.target_model.load_state_dict(self.model.state_dict())
         print(pt_file + ' loaded.')
 
 
 def train(writer, agent, episode):
+    # 训练模型
+
+    # 更新探索率
     agent.update_egreed()
+    # 重开一局
     obs = env.reset()
+    # 这局获得的总奖励数
     total_reward = 0
+    # 每局操作不超过10000次，防止死循环
     for t in range(10000):
+        # 进行一个操作
         act = agent.sample(obs)
         next_obs, reward, done, _ = env.step(act)
 
+        # 保存经验
         trans = Transition(obs, act, reward, next_obs, done)
         agent.store_transition(trans)
 
+        # 每LEARN_FREQ次学习一次
         if t % LEARN_FREQ == 0:
             agent.learn(writer)
 
         obs = next_obs
+        # 累加获得的奖励
         total_reward += reward
         if done or t >= 9999:
+            # 记录这局游戏完成的步骤数
             writer.add_scalar('train/finish_step', t + 1, global_step=episode)
+            # 记录这句游戏获得的总奖励数
             writer.add_scalar('train/total_reward',
                               total_reward, global_step=episode)
             break
 
 
 def evaluate(writer, agent, episode):
-    total_reward = 0
+    # 评估
+
+    # 重开一局
     obs = env.reset()
+    # 这局获得的总奖励数
+    total_reward = 0
+    # 每局操作不超过10000次，防止死循环
     for t in range(10000):
+        # 预测一个操作
         act = agent.predict(obs)
         obs, reward, done, _ = env.step(act)
+
+        # 累加获得的奖励
         total_reward += reward
 
         if done:
+            # 记录这局游戏完成的步骤数
             writer.add_scalar('evaluate/finish_step',
                               t + 1, global_step=episode)
+            # 记录这句游戏获得的总奖励数
             writer.add_scalar('evaluate/total_reward',
                               total_reward, global_step=episode)
             break
 
 
 def main():
+    # 创建智能体
     agent = Agent()
 
+    # 加载训练好的模型文件
     if os.path.exists(pt_file):
         agent.load()
 
+    # 创建tensorboard对象
     writer = SummaryWriter(os.path.join(script_path, 'DQN'))
     for episode in range(EPISODES_NUM):
         if episode % 10 == 0:
             print(f'episode: {episode}')
+
+        # 训练
         train(writer, agent, episode)
+
+        # 每训练10次就评估一次
         if episode % 10 == 9:
             evaluate(writer, agent, episode)
+
+        # 每训练50次就保存模型文件
         if episode % 50 == 49:
             agent.save()
 
--- demos/dqn_flappybird/flappybird_wrapper.py
+++ demos/dqn_flappybird/flappybird_wrapper.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python
 # coding=utf-8
 import numpy as np
-from gym import spaces
+from gym import Env, spaces
 from ple import PLE
 from ple.games import FlappyBird
 
 
-class FlappyBirdWrapper:
+class FlappyBirdWrapper(Env):
+    # 如果想把画面渲染出来，就传参display_screen=True
     def __init__(self, **kwargs):
         self.game = FlappyBird()
         self.p = PLE(self.game, **kwargs)
         self.action_set = self.p.getActionSet()
 
+        # 3个输入状态：见函数self._get_obs
         self.observation_space = spaces.Discrete(3)
+        # 两个输出状态：跳或者不跳
         self.action_space = spaces.Discrete(2)
 
     def _get_obs(self):
+        # 获取游戏的状态
         state = self.game.getGameState()
+        # 小鸟与它前面一对水管中下面那根水管的水平距离
         dist_to_pipe_horz = state["next_pipe_dist_to_player"]
+        # 小鸟与它前面一对水管中下面那根水管的顶端的垂直距离
         dist_to_pipe_bottom = state["player_y"] - state["next_pipe_top_y"]
+        # 获取小鸟的水平速度
         velocity = state['player_vel']
+        # 将这些信息封装成一个数据返回
         return np.array([dist_to_pipe_horz, dist_to_pipe_bottom, velocity])
 
     def reset(self):
@@ -30,4 +38,10 @@ class FlappyBirdWrapper:
         reward = self.p.act(self.action_set[action])
         obs = self._get_obs()
         done = self.p.game_over()
-        return obs, reward, done, None
+        return obs, reward, done, dict()
+
+    def seed(self, *args, **kwargs):
+        pass
+
+    def render(self, *args, **kwargs):
+        pass
